4ba41c7f91e109514222ab98dbbba0784b9277c8
Vaughan Milliman <vaughanm1@protonmail.com>
2021-09-15 20:36:38 -0500

    it works now!

--- json-test.py
+++ json-test.py
@@ -4,14 +4,14 @@ import json
 
 print("JSON test - reading file.json")
 
-json_file = open("file.json", "r")
+with open("file.json", "r") as json_file:
+    json_contents = json_file.read()
+    json_file.close()
 
-print("Contents of file.json - " + json_file.read())
+print("Contents of file.json - " + json_contents)
 
-json_object = json.load(json_file)
+json_object = json.loads(json_contents)
 
-print("JSON value of num-1 - " + json_object["num-1"])
+print("JSON value of num-1 - " + str(json_object["num-1"]))
 print("JSON value of str-1 - " + json_object["str-1"])
-print("JSON values in array-1 - " + json_object["array-1"][0] + ", " + json_object["array-1"][1] + ", " + json_object["array-1"][2])
-
-json_file.close()
+print("JSON values in array-1 - " + str(json_object["array-1"][0]) + ", " + json_object["array-1"][1] + ", " + str(json_object["array-1"][2]))
